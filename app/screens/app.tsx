19c50e04e4f97b5d813be82994bdc59515d497a9
tuposilwe <rudigermkondya@gmail.com>
2025-03-26 20:33:03 +0300

    Added Loading Indicator

--- app/screens/app.tsx
+++ app/screens/app.tsx
@@ -3,6 +3,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { InstalledApps, RNLauncherKitHelper } from "react-native-launcher-kit";
 import { AppDetail } from "react-native-launcher-kit/typescript/Interfaces/InstalledApps";
 import AppGrid from "../components/AppGrid";
+import Loading from "@/components/Loading";
 
 export interface AppState {
   showWithAccent: boolean;
@@ -15,27 +16,33 @@ export interface AppState {
 const App = () => {
   const [apps, setApps] = useState<AppState["apps"]>([]);
   const [showWithAccent, setShowWithAccent] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const initApp = async () => {
+      setIsLoading(true);
+
       try {
         const installedApps = await InstalledApps.getSortedApps({
           includeVersion: true,
           includeAccentColor: true,
         });
 
+        console.log("Installed apps: ", installedApps);
 
-      const appsWithSizes = await Promise.all(
-        apps.map(async (app) => {
-          const size = await InstalledApps.getAppStorageStats({ packageName: app.packageName});
-          const appsized = (size.appBytes/ (1024 * 1024)).toFixed(2) + "MB";
+        const size = await InstalledApps.getAppStorageStats({ packageName: 'com.anonymous.xender'});
+        const appsized = (size.appBytes/ (1024 * 1024)).toFixed(2) + "MB";
 
-          return { ...app, appsized };
-        })
-      );
+      // const appsWithSizes = await Promise.all(
+      //   apps.map(async (app) => {
+      //     const size = await InstalledApps.getAppStorageStats({ packageName: app.packageName});
+      //     const appsized = (size.appBytes/ (1024 * 1024)).toFixed(2) + "MB";
 
-      const size = await InstalledApps.getAppStorageStats({ packageName: 'com.anonymous.xender'});
-      const appsized = (size.appBytes/ (1024 * 1024)).toFixed(2) + "MB";
+      //     return { ...app, appsized };
+      //   })
+      // );
+
+      
 
       console.log("Appsize: ",appsized);
       
@@ -48,11 +55,13 @@ const App = () => {
       // })
       // .catch(error => console.error(error));
 
-      // console.log("Installed apps: ", appsWithSizes);
+     
         
         setApps(installedApps);
       } catch (error) {
         console.error("Error initializing app:", error);
+      }finally{
+        setIsLoading(false);
       }
     };
 
@@ -65,6 +74,11 @@ const App = () => {
       RNLauncherKitHelper.launchApplication(packageName);
     },
   };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
 
   return (
     <View className="flex items-center justify-center">
